92ec7a94a97316698bf64a4b04c63d20ee245dc7
ADIOR-enigma <157355801+ADIOR-enigma@users.noreply.github.com>
2026-04-04 09:57:13 +0530

    Delete theme.js

--- theme.js
+++
@@ -1,6 +0,0 @@
-(() => {
-	const themeScript = document.createElement("SCRIPT");
-	themeScript.setAttribute("type", "text/javascript");
-	themeScript.setAttribute("src", "https://adior-enigma.github.io/Pywalify/theme.script.js");
-	document.head.appendChild(themeScript);
-})();
