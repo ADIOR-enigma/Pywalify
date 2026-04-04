b545e0b4e2d33eebe641be8ab30eda30df7943d3
ADIOR-enigma <157355801+ADIOR-enigma@users.noreply.github.com>
2026-04-04 16:06:42 +0530

    Refactor install.sh to remove theme download
    Removed theme download section and updated echo messages.

--- install.sh
+++ install.sh
@@ -2,25 +2,17 @@
 
 set -e
 
-# Download URL
-theme_url="https://raw.githubusercontent.com/ADIOR-enigma/Pywalify/main"
-
-# Setup directories to download to
+# Get Spicetify base directory
 spice_dir="$(dirname "$(spicetify -c)")"
 theme_dir="${spice_dir}/Themes"
 
-# Make directories if needed
+# Create wal theme directory
 mkdir -p "${theme_dir}/wal"
 
-# Download latest tagged files into correct director
-echo "Downloading wal..."
-curl --silent --output "${theme_dir}/wal/color.ini" "${theme_url}/color.ini"
-echo "Done"
-
 # Apply theme
-echo "Applying theme"
+echo "Applying wal theme..."
 spicetify config current_theme wal color_scheme wal
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
 spicetify apply
 
-echo "All done!"
+echo "Wal theme created"
