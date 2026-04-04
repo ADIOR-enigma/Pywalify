d9894fd3059a61590cfca0eadb9c2b1e6f18a634
ADIOR-enigma <157355801+ADIOR-enigma@users.noreply.github.com>
2026-04-04 16:33:49 +0530

    Simplify spicetify config command
    Removed unnecessary options from spicetify config command.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir -p "${theme_dir}/wal"
 # Apply theme
 echo "Applying wal theme..."
 spicetify config current_theme wal color_scheme wal
-spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
+spicetify config inject_css 1
 spicetify apply
 
 echo "Wal theme created"
